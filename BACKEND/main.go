24eb8a195474437f7148f7ae6e9784dd046d29a5
stkim1 <stkim1@colorfulglue.com>
2016-09-07 00:55:24 +0900

    remove unnecessaries

--- BACKEND/main.go
+++ BACKEND/main.go
@@ -2,19 +2,14 @@ package main
 
 import (
 	"flag"
-	"net/http"
-	//"regexp"
+	"regexp"
 
 	"github.com/golang/glog"
 	"github.com/gorilla/context"
-
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/graceful"
-	"github.com/zenazn/goji/web"
-
 	"github.com/stkim1/BACKEND/framework"
 	"github.com/stkim1/BACKEND/control"
-	"regexp"
 )
 
 func main() {
@@ -26,46 +21,37 @@ func main() {
 	var application = &framework.Application{}
 	application.Init(filename)
 
-	// Setup static files
-	static := web.New()
-	publicPath := application.Config.Get("general.public_path").(string)
-	static.Get("/assets/*", http.StripPrefix("/assets/", http.FileServer(http.Dir(publicPath))))
-
-	http.Handle("/assets/", static)
-
 	// Apply middleware
-	goji.Use(application.ApplySessions)
+	//goji.Use(application.ApplySessions)
 	goji.Use(application.ApplyDbMap)
 	//goji.Use(application.ApplyAuth)
 	goji.Use(application.ApplyIsXhr)
-	goji.Use(application.ApplyCsrfProtection)
+	//goji.Use(application.ApplyCsrfProtection)
 	goji.Use(context.ClearHandler)
 
+	// Setup Routers
 	controller := &control.Controller{}
-
-	// Couple of files - in the real world you would use nginx to serve them.
-	goji.Get("/robots.txt", http.FileServer(http.Dir(publicPath)))
-	goji.Get("/favicon.ico", http.FileServer(http.Dir(publicPath+"/images")))
-
 	// Home page
 	goji.Get("/", application.Route(controller, "Index"))
 	goji.Get("/index.html", application.Route(controller, "Index"))
+
 	goji.Get("/sitemap.xml", application.Route(controller, "Sitemap"))
+
+/*
+	// Sign In routes
+	goji.Get("/signin", application.Route(controller, "SignIn"))
+	goji.Post("/signin", application.Route(controller, "SignInPost"))
+
+	// Sign Up routes
+	goji.Get("/signup", application.Route(controller, "SignUp"))
+	goji.Post("/signup", application.Route(controller, "SignUpPost"))
+
+	// KTHXBYE
+	goji.Get("/logout", application.Route(controller, "Logout"))
+*/
+
+	// Category Index
 	goji.Get(regexp.MustCompile(`^/category/(?P<cat>[a-z]+).html$`), application.Route(controller, "Category"))
-
-	/*
-		// Sign In routes
-		goji.Get("/signin", application.Route(controller, "SignIn"))
-		goji.Post("/signin", application.Route(controller, "SignInPost"))
-
-		// Sign Up routes
-		goji.Get("/signup", application.Route(controller, "SignUp"))
-		goji.Post("/signup", application.Route(controller, "SignUpPost"))
-
-		// KTHXBYE
-		goji.Get("/logout", application.Route(controller, "Logout"))
-	*/
-
 	// Respotory
 	goji.Get(regexp.MustCompile(`^/(?P<repo>[a-z0-9-]+).html$`), application.Route(controller, "Repository"))
 
